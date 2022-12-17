5f962f4e25d757a536d9faf86dbea8204c6e41c3
SaiSatheesh05 <saisatheesh05@gmail.com>
2022-12-17 12:41:27 +0530

    Source Code Push

---
+++ src/org/base/Launch.java
@@ -0,0 +1,22 @@
+package org.base;
+
+public class Launch {
+	public static void main(String[] args) {
+
+		String s = "Kate";
+		System.out.println(s);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
